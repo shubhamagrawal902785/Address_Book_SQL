a2963e9e9b76e62231008b15ff23b05d6907694f
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-12-15 16:08:13 +0530

    Added: Create table

--- src/com/bridglelabz/com/Createdatabase.java
+++ src/com/bridglelabz/com/Createdatabase.java
@@ -5,3 +5,11 @@ create database addressbook;
 show databases;
 
 select database();
+
+// Create table
+
+mysql> create table addressbook(id int unsigned not null auto_increment,  first_name varchar(10), last_name varchar(10), address  varchar(100), city     varchar(20), state    varchar(20),  zip      int ,phone_number     int,email    varchar(30), primary key (id));
+Query OK, 0 rows affected (4.55 sec)
+
+desc addressbook;
+
