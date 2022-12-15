883ae229e351d1e9aa9d101c36c738aeb621cd86
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-12-15 16:33:35 +0530

    Added: insert new data in DB

--- src/com/bridglelabz/com/Createdatabase.java
+++ src/com/bridglelabz/com/Createdatabase.java
@@ -13,3 +13,38 @@ Query OK, 0 rows affected (4.55 sec)
 
 desc addressbook;
 
+mysql> desc addressbook;
++--------------+--------------+------+-----+---------+----------------+
+| Field        | Type         | Null | Key | Default | Extra          |
++--------------+--------------+------+-----+---------+----------------+
+| id           | int unsigned | NO   | PRI | NULL    | auto_increment |
+| first_name   | varchar(10)  | YES  |     | NULL    |                |
+| last_name    | varchar(10)  | YES  |     | NULL    |                |
+| address      | varchar(100) | YES  |     | NULL    |                |
+| city         | varchar(20)  | YES  |     | NULL    |                |
+| state        | varchar(20)  | YES  |     | NULL    |                |
+| zip          | int          | YES  |     | NULL    |                |
+| phone_number | int          | YES  |     | NULL    |                |
+| email        | varchar(30)  | YES  |     | NULL    |                |
++--------------+--------------+------+-----+---------+----------------+
+
+// new data inserting in table
+
+mysql> insert into addressbook (first_name,last_name,address,city,state,zip,phone_number,email)values 
+('shubham','agrawal','mathura','farah','up',281122,902785,'shubham@gmail.com'),('gyanesh','agrawal','farah','mathura','up',2811222,856477,'gya@gmail.com'),
+('yan','pan','cali','ami','us',546895,589745,'yan@gmail.com');
+Query OK, 3 rows affected (0.29 sec)
+Records: 3  Duplicates: 0  Warnings: 0
+
+
+//select data from a database
+
+mysql> select * from addressbook;
++----+------------+-----------+---------+---------+-------+---------+--------------+-------------------+
+| id | first_name | last_name | address | city    | state | zip     | phone_number | email             |
++----+------------+-----------+---------+---------+-------+---------+--------------+-------------------+
+|  2 | shubham    | agrawal   | mathura | farah   | up    |  281122 |       902785 | shubham@gmail.com |
+|  4 | gyanesh    | agrawal   | farah   | mathura | up    | 2811222 |       856477 | gya@gmail.com     |
+|  5 | yan        | pan       | cali    | ami     | us    |  546895 |       589745 | yan@gmail.com     |
++----+------------+-----------+---------+---------+-------+---------+--------------+-------------------+
+3 rows in set (0.03 sec)
