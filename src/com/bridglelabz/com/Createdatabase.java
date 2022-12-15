b6ae1844a2661837ce1b8296a0a8d75456ca97ef
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-12-15 17:11:10 +0530

    Added: add column

--- src/com/bridglelabz/com/Createdatabase.java
+++ src/com/bridglelabz/com/Createdatabase.java
@@ -134,3 +134,53 @@ mysql> select * from addressbook where city='mathura' order by first_name;
 1 row in set (0.04 sec)
 
 mysql> ^C
+
+//add column inDB
+mysql> alter table addressbook add type varchar(50) after last_name;
+Query OK, 0 rows affected (4.82 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> select * from addressbook;
++----+------------+-----------+------+---------+---------+--------------+---------+--------------+-------------------+
+| id | first_name | last_name | type | address | city    | state        | zip     | phone_number | email             |
++----+------------+-----------+------+---------+---------+--------------+---------+--------------+-------------------+
+|  2 | shubham    | agrawal   | NULL | mathura | farah   | up           |  281122 |       902785 | shubham@gmail.com |
+|  4 | gyanesh    | agrawal   | NULL | farah   | mathura | uttarpradesh | 2811222 |       856477 | gya@gmail.com     |
+|  6 | mohit      | goyal     | NULL | mauha   | harda   | raj          |  281789 |       944585 | mohit@gmail.com   |
+|  8 | namen      | tayal     | NULL | majja   | bhun    | urisha       |  284559 |       978985 | name@gmail.com    |
++----+------------+-----------+------+---------+---------+--------------+---------+--------------+-------------------+
+4 rows in set (0.00 sec)
+
+mysql> desc addressbook;
++--------------+--------------+------+-----+---------+----------------+
+| Field        | Type         | Null | Key | Default | Extra          |
++--------------+--------------+------+-----+---------+----------------+
+| id           | int unsigned | NO   | PRI | NULL    | auto_increment |
+| first_name   | varchar(10)  | YES  |     | NULL    |                |
+| last_name    | varchar(10)  | YES  |     | NULL    |                |
+| type         | varchar(50)  | YES  |     | NULL    |                |
+| address      | varchar(100) | YES  |     | NULL    |                |
+| city         | varchar(20)  | YES  |     | NULL    |                |
+| state        | varchar(20)  | YES  |     | NULL    |                |
+| zip          | int          | YES  |     | NULL    |                |
+| phone_number | int          | YES  |     | NULL    |                |
+| email        | varchar(30)  | YES  |     | NULL    |                |
++--------------+--------------+------+-----+---------+----------------+
+10 rows in set (0.13 sec)
+
+mysql> update addressbook set type='profession' where first_name='namen' or first_name='shubham';
+Query OK, 2 rows affected (0.08 sec)
+Rows matched: 2  Changed: 2  Warnings: 0
+
+mysql> select * from addressbook;
++----+------------+-----------+------------+---------+---------+--------------+---------+--------------+-------------------+
+| id | first_name | last_name | type       | address | city    | state        | zip     | phone_number | email             |
++----+------------+-----------+------------+---------+---------+--------------+---------+--------------+-------------------+
+|  2 | shubham    | agrawal   | profession | mathura | farah   | up           |  281122 |       902785 | shubham@gmail.com |
+|  4 | gyanesh    | agrawal   | NULL       | farah   | mathura | uttarpradesh | 2811222 |       856477 | gya@gmail.com     |
+|  6 | mohit      | goyal     | NULL       | mauha   | harda   | raj          |  281789 |       944585 | mohit@gmail.com   |
+|  8 | namen      | tayal     | profession | majja   | bhun    | urisha       |  284559 |       978985 | name@gmail.com    |
++----+------------+-----------+------------+---------+---------+--------------+---------+--------------+-------------------+
+4 rows in set (0.00 sec)
+
+mysql>
